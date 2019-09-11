b1f9ba7e6770f687a810667c3001de97c4d05978
sheepover96 <sheep.over@gmail.com>
2019-09-12 02:28:51 +0900

    add tw link

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -11,6 +11,7 @@
         <a target="_blank" href="https://github.com/sheepover96/" class="logo"><i class="fab fa-github fa-lg"></i></a>
         <a target="_blank" href="https://www.linkedin.com/in/%E7%9C%9F%E4%B8%80%E5%B8%8C-%E5%A4%A7%E5%80%89-a17397149/?locale=en_US" class="logo"><i class="fab fa-linkedin-in fa-lg"></i></a>
         <a target="_blank" href="https://www.facebook.com/profile.php?id=100014972891268"  class="logo"><i class="fab fa-facebook fa-lg"></i></a>
+        <a target="_blank" href="https://twitter.com/LovelyQuoteBot"  class="logo"><i class="fab fa-twitter fa-lg"></i></a>
         <a target="_blank" href="https://serenard.hatenablog.com/"  class="logo"><i class="fa fa-rss"></i></a>
         <a target="_blank" href="https://drive.google.com/open?id=11ixcpV4PIs-wwdbG0yKlvEVeE0yifK0-"  class="logo"><i class="fas fa-file"></i></a>
       </div>
